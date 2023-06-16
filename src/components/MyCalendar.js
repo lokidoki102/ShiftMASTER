4cad4ac5a1cbad3082699175315ca3e4f3cd80ee
carl <e0959394@u.nus.edu>
2023-06-16 22:11:13 +0800

    adding event to firebase (unfinished)

--- src/components/MyCalendar.js
+++ src/components/MyCalendar.js
@@ -5,30 +5,51 @@ import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import { useRef } from "react";
-import { collection, query, where, orderBy, onSnapshot } from 'firebase/firestore';
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+  addDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
-
+import { Modal } from "react-bootstrap";
 
 const DnDCalendar = withDragAndDrop(Calendar);
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = () => {
-
   const [events, setEvents] = useState([]);
-//   const [startDate, setStartDate] = useState(null);
-//   const [endDate, setEndDate] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
+  // Modal
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  // Pagination for fetching events
   useEffect(() => {
     const todayDate = new Date(); // Set the initial visible start date
-    const initialStartDate = moment(todayDate).subtract(16, 'days').toDate(); // Show 31 days
-    const initialEndDate = moment(todayDate).add(16, 'days').toDate(); // Show 31 days
+    const initialStartDate = moment(todayDate).subtract(16, "days").toDate(); // Show 31 days
+    const initialEndDate = moment(todayDate).add(16, "days").toDate(); // Show 31 days
 
     fetchEvents(initialStartDate, initialEndDate);
   }, []);
 
+  //  Query for shifts
   const fetchEvents = useCallback((start, end) => {
-    const eventsRef = collection(db, 'shift');
-    const q = query(eventsRef, where('start', '>=', start), where('start', '<', end), orderBy('start'));
+    const eventsRef = collection(db, "shift");
+    const q = query(
+      eventsRef,
+      where("start", ">=", start),
+      where("start", "<", end),
+      orderBy("start")
+    );
 
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const fetchedEvents = [];
@@ -42,7 +63,6 @@ const MyCalendar = () => {
         });
         console.log("printing title:");
         console.log(eventData.title);
-
       });
 
       setEvents(fetchedEvents);
@@ -51,9 +71,9 @@ const MyCalendar = () => {
     return () => unsubscribe();
   }, []);
 
-  const handleRangeChange = useCallback((start, end) => {
-    fetchEvents(start, end);
-  }, [fetchEvents]);
+  //   const handleRangeChange = useCallback((start, end) => {
+  //     fetchEvents(start, end);
+  //   }, [fetchEvents]);
 
   // event handlers for calendar
   const onEventDrop = (data) => {
@@ -82,9 +102,31 @@ const MyCalendar = () => {
     setEvents(updatedEvents);
   };
 
-  const onSelecting = ({ start, end }) => {
-    console.log("Selecting:", start, end);
-  };
+  const onSelectSlot = useCallback((slotInfo) => {
+    console.log(slotInfo.start);
+    console.log(slotInfo.end);
+    openModal();
+  }, []);
+
+
+  //   const onSelectSlot = async ({ start, end }) => {
+  //     const newShift = {
+  //         title: 'New Shift',
+  //         start,
+  //         end,
+  //       };
+
+  //       try {
+  //         // Add the new event to Firestore
+  //         const docRef = await addDoc(collection(db, 'shift'), newShift);
+  //         console.log('Event added with ID:', docRef.id);
+
+  //         // Fetch the updated events from Firestore
+  //         fetchEvents(start, end);
+  //       } catch (error) {
+  //         console.error('Error adding event:', error);
+  //       }
+  //   };
 
   return (
     <div>
@@ -96,8 +138,8 @@ const MyCalendar = () => {
         draggableAccessor={(event) => true}
         onEventDrop={onEventDrop}
         onEventResize={onEventResize}
-        onSelecting={onSelecting}
-        //   onSelectSlot={this.onSelectSlot}
+        // onSelecting={onSelecting}
+        onSelectSlot={onSelectSlot}
         selectable
         style={{
           height: "500px",
@@ -107,6 +149,12 @@ const MyCalendar = () => {
           display: "flex",
         }}
       />
+
+      <Modal isOpen={showModal} onRequestClose={closeModal}>
+        <h2>Modal Title</h2>
+        <p>Modal content goes here...</p>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
     </div>
   );
 }; // end of MyCalendar
